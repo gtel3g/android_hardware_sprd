d6c33e6c97a98d55d55f6b1f8892da66125a8f87
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-06 23:07:58 +0300

    libshims: fix GPS OpenSSL compatibility shim on Pie

--- libshims/gps_shim.cpp
+++ libshims/gps_shim.cpp
@@ -46,9 +46,15 @@ extern "C" int _ZNK7android13SensorManager17assertStateLockedEv() {
 
 extern "C" void *CRYPTO_malloc(int num, const char *file, int line);
 extern "C" void *CRYPTO_malloc(int num, const char *file, int line) {
+    (void)file;
+    (void)line;
     return num <= 0 ? NULL : malloc(num);
 }
 
 extern "C" long SSL_CTX_ctrl(SSL_CTX *ctx, int cmd, long larg, void *parg) {
+    (void)ctx;
+    (void)cmd;
+    (void)larg;
+    (void)parg;
     return 0;
 }
