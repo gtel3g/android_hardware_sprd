c502869b538f3bd2939dd60ba441839ac44ad89b
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-06 21:49:31 +0300

    healthd: mark unused board callback parameters

--- healthd/healthd.cpp
+++ healthd/healthd.cpp
@@ -31,26 +31,26 @@ healthd_board_init(struct healthd_config *config)
 }
 
 int
-healthd_board_battery_update(struct android::BatteryProperties *props)
+healthd_board_battery_update(struct android::BatteryProperties* /* props */)
 {
     // Return non-zero to prevent logging battery status heartbeat message to the kernel log
     return 1;
 }
 
 void
-healthd_board_mode_charger_draw_battery(struct android::BatteryProperties* batProps)
+healthd_board_mode_charger_draw_battery(struct android::BatteryProperties* /* batProps */)
 {
 
 }
 
 void
-healthd_board_mode_charger_battery_update(struct android::BatteryProperties* batProps)
+healthd_board_mode_charger_battery_update(struct android::BatteryProperties* /* batProps */)
 {
 
 }
 
 void
-healthd_board_mode_charger_set_backlight(bool on)
+healthd_board_mode_charger_set_backlight(bool /* on */)
 {
 
 }
